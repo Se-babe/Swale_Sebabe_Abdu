c321331920b510c8246f8b55bc41edf7081c1986
Se-babe <xwalesebabe@gmail.com>
2025-06-04 16:06:36 +0300

    added new files and updated others

---
+++ MachineLearning/learn_machinelearning.py
@@ -0,0 +1,41 @@
+#machine learning
+#introduction to machine learning
+#what is machine learning?
+#it refers to the use of algorithms and statistical models to enable computers to perform tasks without explicit instructions, relying on patterns and inference instead.
+#is the branch of artificial intelligence (AI) that focuses on the development of systems that can learn from and make decisions based on data.
+#machine learning is a subset of artificial intelligence (AI) that focuses on the development of algorithms and statistical models that enable computers to perform tasks without explicit instructions, relying on patterns and inference instead.
+#Application of machine learning includes:
+#1. Image and speech recognition
+#2. Natural language processing
+#3. Predictive analytics
+#4. Autonomous vehicles
+#5. Recommendation systems
+#6.Disease diagnosis
+#7. Fraud detection
+#8.stock market analysis
+
+#types of machine learning:
+#1. Supervised learning: Involves training a model on labeled data, where the input data is paired with the correct output. The model learns to map inputs to outputs and can make predictions on new, unseen data.
+
+'''common algorithms in supervised learning include:
+- Linear Regression
+- Logistic Regression
+- Decision Trees
+- Support Vector Machines (SVM)
+- Neural Networks
+- k-Nearest Neighbors (k-NN)
+- Random Forests
+
+-Deep learning: image classification A subset of machine learning that uses neural networks with many layers (deep neural networks) to model complex patterns in large datasets. It is particularly effective for tasks like image and speech recognition.
+
+'''
+#2. Unsupervised learning: Involves training a model on unlabeled data, where the model tries to learn the underlying patterns and structures in the data without explicit guidance on what the outputs should be.
+'''  -k-means clustering: A method that partitions data into k distinct clusters based on feature similarity.
+     - Hierarchical clustering: A method that builds a hierarchy of clusters by either merging smaller clusters into larger ones or splitting larger clusters into smaller ones.
+     - Principal Component Analysis (PCA): A dimensionality reduction technique that transforms data into a lower-dimensional space while preserving as much variance as possible.
+     - Association rule learning: A method that discovers interesting relationships between variables in large datasets, often used in market basket analysis.'''
+     #model selectiom and validation
+     
+#3. Reinforcement learning: Involves training a model to make a sequence of decisions by rewarding desired behaviors and penalizing undesired ones. The model learns to optimize its actions based on feedback from the environment.
+''' training a model giving out rewards'''
+#4. Semi-supervised learning: Combines both labeled and unlabeled data to improve learning accuracy. It uses a small amount of labeled data along with a larger amount of unlabeled data.
