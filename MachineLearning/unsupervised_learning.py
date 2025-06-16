9a76db0146409d501a2d2f86f44305c8318a7e26
Se-babe <xwalesebabe@gmail.com>
2025-06-16 15:04:07 +0300

    updates

---
+++ MachineLearning/unsupervised_learning.py
@@ -0,0 +1,31 @@
+#unsupervised learning
+#what is unsupervised learning
+
+#characteristics of unsupervised leanring
+#no labelling of data provided
+#the model identity hidden patterns ,structures
+#used for EDA exploratory data analysis
+#can handle large volumes of data
+
+#techniques
+#clustering
+#Dimensionality reduction
+#Anomaly Detection
+# Association Rule Learning
+
+
+#clustering
+#algorithms for clustering
+#k-means clustering
+#hierarchical clustering
+#density based spartial clustering (DBSCAN)
+
+#advantages of unsupervised learning
+#useful for discovering unknown patterns
+#no need for labelling data
+#can handle large  ,complex data
+#flexible for EDA
+
+#example one: Real problem
+#create a data set to cluster customers based on their anual income and spending
+#k - means clustering
