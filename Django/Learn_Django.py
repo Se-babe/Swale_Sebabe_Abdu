f07d42143f7dd099b249842d4ca273ae3adb56a0
Se-babe <xwalesebabe@gmail.com>
2025-06-23 11:04:01 +0300

    updates

---
+++ Django/Learn_Django.py
@@ -0,0 +1,16 @@
+#Building web Application using Django
+#https://www.djangoproject.com/
+
+#parking page #landing page #lovable vibe coding prototypes
+#steps to create a web app in Django
+#1.create virtual env
+#3.install Django
+#3create django project
+#4.test if Django is installed
+#5.cretae apps 
+
+#all web apps have a login sign up #KYC #data collection #interlectual property
+
+#create templates
+#run server and migration
+#
